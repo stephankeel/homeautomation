2d13348fb5603392b638286541aa82194856857c
stephankeel <stephan.keel@hsr.ch>
2016-11-24 15:40:06 +0100

    logout

--- src/app/login.component.ts
+++ src/app/login.component.ts
@@ -16,18 +16,26 @@ export class LoginComponent {
     password: string;
 
     loginInfo: string;
+    loggedOut: boolean = true;
 
     constructor(private genericService: GenericService,
                 private router: Router) {
+        this.loggedOut = !genericService.loggedIn();
     }
 
     doLogin(): void {
         this.genericService.login(this.username, this.password).then(user => {
             this.loginInfo = 'welcome';
+            this.loggedOut = false;
             this.router.navigate(['/dashboard']);
         }).catch(error => {
             this.loginInfo = 'login failure: ' + (error.message || error);
         });
+    }
+
+    doLogout(): void {
+        this.genericService.logout();
+        this.loggedOut = true;
     }
 
 }
